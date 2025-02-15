eafb09d1389eb2c30d1b677b7c530a2071a37452
EJ <sbjdnm@gmail.com>
2025-02-16 00:24:37 +0900

    feat: 모달 백그라운드 클릭시 close 기능 추가 & 엔터키 감지 로직 추가

--- src/app/main/components/TaskModal.tsx
+++ src/app/main/components/TaskModal.tsx
@@ -19,9 +19,23 @@ export const TaskModal = ({
 }: TaskModalProps) => {
   if (!isOpen) return null;
 
+  // 엔터 키 입력 시 실행
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      onSave();
+    }
+  };
+
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white p-6 rounded-lg shadow-md w-80">
+    <div
+      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 text-slate-600"
+      onClick={onClose} // 뒷배경 클릭 시 닫기
+    >
+      <div
+        className="bg-white p-6 rounded-lg shadow-md w-80"
+        onClick={(e) => e.stopPropagation()} // 모달 클릭 시 닫히지 않도록 방지
+      >
         <h2 className="text-lg font-bold mb-2">
           {isEditing ? "할 일 수정" : `${selectedStatus}에 할 일 추가`}
         </h2>
@@ -30,6 +44,7 @@ export const TaskModal = ({
           placeholder="할 일을 입력하세요"
           value={taskInput}
           onChange={(e) => setTaskInput(e.target.value)}
+          onKeyDown={handleKeyDown} // 엔터 입력 감지
           className="w-full border p-2 rounded mb-2"
         />
         <div className="flex justify-end gap-2">
